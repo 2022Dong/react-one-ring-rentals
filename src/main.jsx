29d1e3a0548557d2bd7fe9d6661090e5105c9906
2022Dong <faithhopelove777111@gmail.com>
2024-10-07 14:59:44 +0800

    init: style

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// Importing Bootstrap CSS files
+import './assets/css/bootstrap.css';
+import './assets/css/bootstrap.min.css';
+
+// Importing FontAwesome CSS files
+import './assets/css/font-awesome.css';
+import './assets/css/font-awesome.min.css';
+
+// Importing custom styles
+import './assets/css/font.css';
+import './assets/css/owl.transitions.css';
+import './assets/css/style.css';
+import './assets/css/style.less';  // npm install less less-loader
+
+// Import Bootstrap Datepicker (if needed)
+import './assets/css/bootstrap-datepicker.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
